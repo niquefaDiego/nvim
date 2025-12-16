3f17f5f2a8e8fbeddd85e51e75db18be478986da
Diego Niquefa <niquefa.diego@gmail.com>
2025-12-15 20:55:34 -0800

    Use mason-lspconfig instead of mason + lsp

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "lua_ls",
+                "rust_analyzer"
+            }
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            {
+                "neovim/nvim-lspconfig",
+                config = function()
+                end,
+                dependencies = {
+                    "folke/lazydev.nvim",
+                    ft = "lua", -- only load on lua files
+                    opts = {
+                        library = {
+                            -- See the configuration section for more details
+                            -- Load luvit types when the `vim.uv` word is found
+                            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                        },
+                    },
+                }
+            }
+        },
+    }
+}
