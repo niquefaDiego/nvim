87ec43219ce02ea8156773f0f6a6d67d3cdb0be8
Diego Niquefa <niquefa.diego@gmail.com>
2026-01-03 20:27:46 -0800

    Fix formatting

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -20,26 +20,27 @@ return {
                 delay = {
                     highlight = 0, -- Delay between jump and highlighting all possible jumps
                     idle_stop = 10000000, -- Delay between jump and automatic stop if idle (no jump is done)
-                }})
-                require('mini.jump2d').setup()
-                require('mini.completion').setup()
-                require('mini.sessions').setup()
-                require('mini.visits').setup()
-                require('mini.splitjoin').setup({
-                    mappings = {
-                        toggle = 'ga',
-                        split = '',
-                        join = '',
-                    }
-                })
-                require('mini.comment').setup({
-                    mappings = {
-                        comment = 'gc', -- Toggle comment in normal and visual mode
-                        comment_line = 'gc', -- Toggle comment on current line
-                        comment_visual = 'gc', -- Toggle comment on visual selection
-                        textobject = 'gc', -- Define 'comment' textobject, so that 'dgc' works.
-                    },
-                })
-            end
-        }
+                }
+            })
+            require('mini.jump2d').setup()
+            require('mini.completion').setup()
+            require('mini.sessions').setup()
+            require('mini.visits').setup()
+            require('mini.splitjoin').setup({
+                mappings = {
+                    toggle = 'ga',
+                    split = '',
+                    join = '',
+                }
+            })
+            require('mini.comment').setup({
+                mappings = {
+                    comment = 'gc', -- Toggle comment in normal and visual mode
+                    comment_line = 'gc', -- Toggle comment on current line
+                    comment_visual = 'gc', -- Toggle comment on visual selection
+                    textobject = 'gc', -- Define 'comment' textobject, so that 'dgc' works.
+                },
+            })
+        end
     }
+}
